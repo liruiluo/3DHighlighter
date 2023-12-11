ea729bb3c4e292697564d3e7e266222cf753893b
liruiluo <l1176358062@gmail.com>
2023-12-11 15:10:40 +0800

    use self-consistency for stable optimize

---
+++ demo/run_dog_necklace_avg.sh
@@ -0,0 +1,1 @@
+python main_avg.py --obj_path data/dog.obj --output_dir results/demo_dog --seed 0 --classes necklace --object dog --n_iter 2500 --frontview_center 0 0
---
+++ demo/run_horse_necklace.sh
@@ -0,0 +1,1 @@
+python main.py --obj_path data/horse.obj --output_dir results/demo_horse --seed 1 --classes necklace --object horse --n_iter 2500 --frontview_center 0 0 --frontview_std 1
